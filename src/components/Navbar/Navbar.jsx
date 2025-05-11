99061b8bc921b9b5a7fde8c10f68481fc31bf687
salimahmedmuhammed98765@gmail.com <salimahmedmuhammed98765@gmail.com>
2025-05-11 15:04:41 +0300

    creating navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,40 @@
 import styles from './Navbar.module.css'
+import logo from '../../assets/logo.png'
+import searchIcon from '../../assets/search_icon.svg';
+import bellIcon from '../../assets/bell_icon.svg';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AccountBoxIcon from '@mui/icons-material/AccountBoxOutlined';
 
 function Navbar() {
   return (
     <div className={styles.navbar}>
-      Header
+      <div className={styles.leftCon}>
+        <img src={logo} alt="Logo" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+
+      <div className={styles.rightCon}>
+        <img src={searchIcon} alt="Search Icon" className={styles.icons} />
+        <img src={bellIcon} alt="Bell Icon" className={styles.icons} />
+
+        <div className={styles.profileCon}>
+          <AccountBoxIcon className={styles.profile} />
+          <ArrowDropDownIcon />
+          {/* Logout text */}
+          <div className={styles.dropdown}>
+            <p>Sign Out</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
