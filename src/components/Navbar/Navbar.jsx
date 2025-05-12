cf8118810841b2f41e0fd4fdeb4fae3bd02af186
salimahmedmuhammed98765@gmail.com <salimahmedmuhammed98765@gmail.com>
2025-05-12 10:27:33 +0300

    make the background of navbar darken when scroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,23 @@ import searchIcon from '../../assets/search_icon.svg';
 import bellIcon from '../../assets/bell_icon.svg';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import AccountBoxIcon from '@mui/icons-material/AccountBoxOutlined';
+import { useEffect, useRef } from 'react';
 
 function Navbar() {
+  const navRef = useRef();
+
+  useEffect(() => { 
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 80) {
+        navRef.current.classList.add(styles.nav_dark);
+      } else {
+        navRef.current.classList.remove(styles.nav_dark);
+      }
+    });
+  }, []);
+
   return (
-    <div className={styles.navbar}>
+    <div ref={navRef} className={styles.navbar}>
       <div className={styles.leftCon}>
         <img src={logo} alt="Logo" />
         <ul>
